f9c82d4b675943f409bdc86165beaecd3421f4e4
PaulErnest <PaulErMartin@yahoo.fr>
2025-03-10 18:32:18 +0100

    *add : suffixes

---
+++ demo/1_Core/suffixes_litteraux_conversion.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+double operator"" _K(long double celsius) {
+    return celsius + 273.15; // Conversion °C → K
+}
+
+int main() {
+    double temp = 25.0_K;
+    std::cout << "Temperature en Kelvin : " << temp << " K" << std::endl; // Affiche 298.15 K
+    return 0;
+}
