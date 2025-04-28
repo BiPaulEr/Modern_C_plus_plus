e06faaa6c7f91608fd1a749d01f6ca1d97289185
PaulErnest <PaulErMartin@yahoo.fr>
2025-04-28 16:34:03 +0200

    *add : visit varaint

---
+++ correction/5_Generics/4_visit_variant.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <variant>
+#include <string>
+
+struct Car {
+    void start() const { std::cout << "Car is starting.\n"; }
+    void stop() const { std::cout << "Car is stopping.\n"; }
+};
+
+struct Bike {
+    void start() const { std::cout << "Bike is starting.\n"; }
+    void stop() const { std::cout << "Bike is stopping.\n"; }
+};
+
+struct Boat {
+    void start() const { std::cout << "Boat is starting.\n"; }
+    void stop() const { std::cout << "Boat is stopping.\n"; }
+};
+
+// Create a concept StartStopable for Bike / Boat / Car 
+template <typename T>
+concept StartStopable = requires(T a) {
+    { a.start() } -> std::same_as<void>;
+    { a.stop() } -> std::same_as<void>;
+};
+
+// Create a variant Vehicule
+using Vehicle = std::variant<Car, Bike, Boat>;
+
+//Complete the template startVehicle
+template <StartStopable T>
+void startVehicle(const T& vehicle) {
+    vehicle.start();
+    std::cout << "Vehicle started.\n";
+}
+
+//Complete the template stopVehicle
+template <StartStopable T>
+void stopVehicle(const T& vehicle) {
+    vehicle.stop();
+    std::cout << "Vehicle stopped.\n";
+}
+
+// complete the function to start and stop the vehicule
+void test(const Vehicle& vehicle) {
+    std::visit([](const auto& v) {
+        startVehicle(v);
+        stopVehicle(v);
+        }, vehicle);
+}
+
+int main() {
+    // Create vehicles
+    Vehicle car = Car{};
+    Vehicle bike = Bike{};
+    Vehicle boat = Boat{};
+
+    // Dispatch the vehicles to start and stop
+    test(car);
+    test(bike);
+    test(boat);
+
+    return 0;
+}
---
+++ exercice/5_Generics/4_visit_variant.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <variant>
+#include <string>
+
+struct Car {
+    void start() const { std::cout << "Car is starting.\n"; }
+    void stop() const { std::cout << "Car is stopping.\n"; }
+};
+
+struct Bike {
+    void start() const { std::cout << "Bike is starting.\n"; }
+    void stop() const { std::cout << "Bike is stopping.\n"; }
+};
+
+struct Boat {
+    void start() const { std::cout << "Boat is starting.\n"; }
+    void stop() const { std::cout << "Boat is stopping.\n"; }
+};
+
+// Create a concept StartStopable for Bike / Boat / Car 
+template <typename T>
+concept StartStopable = requires(T a) {
+    { a.start() } -> std::same_as<void>;
+    { a.stop() } -> std::same_as<void>;
+};
+
+// Create a variant Vehicule
+using Vehicle = std::variant<Car, Bike, Boat>;
+
+//Complete the template startVehicle
+
+void startVehicle(const T& vehicle) {
+
+}
+
+//Complete the template stopVehicle
+
+void stopVehicle(const T& vehicle) {
+
+}
+
+// complete the function to start and stop the vehicule
+void test(const Vehicle& vehicle) {
+
+}
+
+int main() {
+    Vehicle car = Car{};
+    Vehicle bike = Bike{};
+    Vehicle boat = Boat{};
+
+    test(car);
+    test(bike);
+    test(boat);
+    return 0;
+}
